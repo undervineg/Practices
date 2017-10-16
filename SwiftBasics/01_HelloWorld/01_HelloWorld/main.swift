2454c23c857ba8f2712ebf2f029557e42f2143d7
undervine <undervine@naver.com>
2017-10-16 15:34:16 +0900

    folder changed

---
+++ SwiftBasics/01_HelloWorld/01_HelloWorld/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  01_HelloWorld
+//
+//  Created by 심 승민 on 2017. 10. 16..
+//  Copyright © 2017년 심 승민. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
