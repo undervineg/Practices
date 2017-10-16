d6229642585acac2bc9e8712a7a240009ae1a400
undervine <undervine@naver.com>
2017-10-16 17:58:54 +0900

    inner function made

--- SwiftBasics/Day1/UnitConverter/UnitConverter/main.swift
+++ SwiftBasics/Day1/UnitConverter/UnitConverter/main.swift
@@ -9,32 +9,37 @@
 import Foundation
 
 // 단위 변환기
-func lengthConverter(inputLength: String) -> String{
-    let divider: Int = 100
-    var doubleDivider: Double { return Double(divider) }
+func lengthConverter(inputLength: String)->String{
+    let divider: Double = 100.0
     var result: String = ""
     var inputLength = inputLength
+    var inputDouble: Double{ return Double(inputLength)! }
+    
+    func centimeterToMeter(inputNumber: Double)->Double{
+        return Double(inputNumber / divider)
+    }
+    
+    func meterToCentimeter(inputNumber: Double)->Double{
+        return Double(inputNumber * divider)
+    }
     
     if inputLength.contains("cm"){
         for _ in 0..<2{
             inputLength.removeLast()
         }
-        if let inputLength = Double(inputLength){
-            result = String(inputLength / doubleDivider)
-        }
-        result += "m"
-    }else if inputLength.contains("m"){
+        result = String(centimeterToMeter(inputNumber: inputDouble))+"m"
+    }
+    else if inputLength.contains("m"){
         inputLength.removeLast()
-        if let inputLength = Double(inputLength){
-            result = String(inputLength * doubleDivider)
-        }
-        result += "cm"
-    }else{
+        result = String(meterToCentimeter(inputNumber: inputDouble))+"cm"
+    }
+    else{
         result = "none"
     }
     
     return result
 }
+
 
 // 사용자 입력을 받아 결과 출력
 while(true){
