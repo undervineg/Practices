f1736ed9d5465f3275301f646327cf2bdc7072c6
undervine <undervine@naver.com>
2017-10-16 15:52:24 +0900

    README.md modified

--- SwiftBasics/GithubTest/GithubTest/main.swift
+++ SwiftBasics/GithubTest/GithubTest/main.swift
@@ -9,16 +9,3 @@
 import Foundation
 
 print("Hello, iOS World!")
-
-/*
- 
- # 깃헙 테스트 입니다.
- ## 요구사항
- > iOS 프로그램을 개발할 수 있는 소프트웨어 개발 도구(Software Development Kit, 이하 SDK)를 포함하고 있는 Xcode를 설치한다.
- > Xcode 실행하고 새로운 Playground를 생성한 후 "Hello, iOS World" 메시지를 출력하는 프로그램을 구현하고 실행한다.
- > 구현한 소스 코드를 저장하고 관리하기 위한 github 저장소를 생성한다.
- > github 저장소에 위키 페이지를 만들고 markdown 문법으로 문서를 작성한다.
- > Playground 와 github 저장소를 연결해 소스 코드를 저장소에 추가한다.
- > Playground 에서 소스코드를 수정한 후 수정된 내용을 github 저장소에 반영한다.
- 
- */
