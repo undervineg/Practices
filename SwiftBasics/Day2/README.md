fd3551105a56e2e3dc3decb4f47aa7b33267620e
undervine <undervine@naver.com>
2017-10-17 14:25:53 +0900

    struct, dictionary 활용 및 inch 단위 추가

---
+++ SwiftBasics/Day2/README.md
@@ -0,0 +1,25 @@
+# 간접참조
+컴퓨터 공학에서 "간접 참조"는 값 자체보다 컨테이너, 연결, 별명 등을 사용해서 우회해서 참조하도록 하는 방식을 말함
+
+
+# 이미 만들어놓은 함수들
+print, zip, swap, stride, readLine, min, max, abs, assert, assertionFailure, debugPrint, dump, fatalError, getVaList, isKnownUniquelyReferenced, numericCast, precondition, preconditionFailure, repeatElement, sequence, stride, transcode, unsafeBitCast, unsafeDowncast, withExtendedLifetime, withUnsafeMutablePointer, withUnsafePointer, withVaList를 자주 씀
+
+
+# 데이터 타입
+swift에서는 Int, String, Double, Bool 등의 원시타입이 구조체로 구현되어 있다.
+
+
+# 함수
+선언부와 호출부는 항상 나뉘게 될 것이다.
+
+
+# 문자열
+## Search Strings (검색)
+> Range를 자주 씀. 범위연산자.
+
+## A View of String
+> String.unicodeScalars
+
+# 옵셔널
+박스로 값을 감싼 형태
---
+++ SwiftBasics/Day2/UnitConverter/UnitConverter/main.swift
@@ -0,0 +1,62 @@
+//
+//  main.swift
+//  UnitConverter
+//
+//  Created by 심 승민 on 2017. 10. 16..
+//  Copyright © 2017년 심 승민. All rights reserved.
+//
+
+import Foundation
+
+let lengthUnit: [String:Double] = ["cm" : 1, "m" : 100, "inch" : 2.54]
+
+// 길이를 표현하는 구조체로, 값과 단위로 이루어져 있다.
+struct Length{
+    var val: Double
+    var unit: String
+    // 파라미터로 넘겨받은 단위로 변환하는 함수
+    func convert(to unit: String)->Length{
+        // 현재 val 값을 cm 기준으로 변환한 후 바꿀 unit 값으로 나눈 값을 새 Length를 만들어 반환
+        guard let selfUnit = lengthUnit[self.unit], let toUnit = lengthUnit[unit] else{ return Length(val: 0, unit: "") }
+        return Length(val: self.val * selfUnit / toUnit, unit: unit)
+    }
+    func toString()->String{
+        return String(self.val) + unit
+    }
+}
+
+// 단위 변환기
+func convertLength(_ length: String, to unit: String) -> String {
+    var currUnit: String = ""
+    
+    // 파라미터로 받은 문자열에서 단위부 탐색
+    for (key, _) in lengthUnit {
+        if length.hasSuffix(key){
+            currUnit = key
+            break   // 'cm'에서 'm'가 인식 되는 등 중복 탐색을 방지하기 위하여 첫 탐색 후 for문 나옴
+        }
+    }
+    
+    // 숫자부와 단위부로 나누어 Length 타입으로 변환
+    // index(of)로 구한 index는 옵셔널 타입이기 때문에 범위연산자에 넣을 때 언래핑해야 함!
+    if let firstIndexOfcurrUnit = currUnit.first,
+        let currUnitIndexInLength = length.index(of: firstIndexOfcurrUnit), 
+        let currDigitDouble = Double(length[..<currUnitIndexInLength]) {
+        // Length 타입의 convert() 함수를 호출하여 단위 변환
+        return Length(val: currDigitDouble, unit: currUnit).convert(to: unit).toString()
+    }
+    
+    return ""
+}
+
+
+// 사용자 입력을 받아 결과 출력
+while(true){
+    print("길이(단위포함)를 변환할 단위와 함께 입력해주세요:", terminator: " ")
+    if let inputLine = readLine(){
+        let separatedInput = inputLine.split(separator: " ")
+        let trimmedInput = separatedInput.map({$0.trimmingCharacters(in: .whitespaces)})
+        let result = convertLength(trimmedInput[0], to: trimmedInput[1])
+        print(result)
+    }
+}
