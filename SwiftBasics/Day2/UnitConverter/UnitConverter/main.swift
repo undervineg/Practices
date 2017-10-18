e117144609d9a39c0a10ae42ab4decb1e0ebf49a
undervine <undervine@naver.com>
2017-10-18 12:15:16 +0900

    comments updated

--- SwiftBasics/Day2/UnitConverter/UnitConverter/main.swift
+++ SwiftBasics/Day2/UnitConverter/UnitConverter/main.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 // 지원 단위 및 최소단위(cm,g,L) 기준 변환값.
-// static을 쓸 수 없는 이유는 전역변수이기 때문인가?
+// static을 쓸 수 없는 이유는 전역변수이기 때문인가? 추후 따로 다른 파일에 옮겨서 사용.
 let lengthUnit: [String:Double] = ["cm" : 1, "m" : 100, "inch" : 2.54, "yard" : 91.44]
 let weightUnit: [String:Double] = ["g" : 1, "kg" : 1000, "lb" : 453.592, "oz" : 28.3495]
 let volumeUnit: [String:Double] = ["L" : 1, "pt" : 0.473176, "qt" : 0.946353, "gal" : 3.78541]
